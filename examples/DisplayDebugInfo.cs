7177ce28b3578066c5255216affdf6ec1c4443c7
Jakub Šturc <b@NB-ESK-STU.erni2.ch>
2019-09-17 09:01:56 +0200

    Add DisplayDebugInfo class

---
+++ examples/DisplayDebugInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace JakubSturc.Talks.UnicodeWithDotNet
+{
+    public class DisplayDebugInfo : ICommand
+    {
+        public string Code => "debug";
+
+        public string Name => "Display debug info";
+
+        public void Run()
+        {
+            Console.WriteLine($"Current process ID is {Process.GetCurrentProcess().Id}");
+            Console.WriteLine($"Current directory is '{Directory.GetCurrentDirectory()}'");
+        }
+    }
+}
